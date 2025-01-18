b4ec266c9bc1f3ae3ffa69eadbabe3898382674d
RandyLiu65 <randyl65@nycstudents.net>
2025-01-18 05:08:13 +0000

    Calculator Partner Challenge

--- src/main/java/com/example/project/CalculatorRunner.java
+++ src/main/java/com/example/project/CalculatorRunner.java
@@ -3,10 +3,30 @@ package com.example.project;
 public class CalculatorRunner {
     public static void main(String[] args) {
 
+
         //test your program here
         //1. create an instance of the calculator class
+        Calculator cal = new Calculator("Cal1");
+
 
         //2. call any methods of that class to test
+        double operation = cal.performOperation("+", 5,6);
+        System.out.println(operation);
+       
+        boolean trueFalse= cal.divisibleBy(5, 6);
+        System.out.println(trueFalse);
 
+
+        String coords = cal.coordinatePair(5, 6);
+        System.out.println(coords);
+       
+        int absValue = cal.absoluteValue(5, 6);
+        System.out.println(absValue);
+       
+        String information = cal.info();
+
+
+       
     }
 }
+
